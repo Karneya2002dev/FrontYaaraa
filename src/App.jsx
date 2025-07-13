da3451469cd2437a66da60f3eabce38b307a6f57
Karneyaprojects <karneya.r02@gmail.com>
2025-07-13 21:15:31 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
       <NavBar />
       <div className="pt-16">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
